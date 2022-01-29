604fc598a04a08339d9b3b2eb8e4f5e1c308f534
Alex Ferro <alex@Alexs-Mac-mini.local>
2022-01-29 15:33:21 -0500

    modifying search func

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <div className='App'>
+      <Coin />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Coin/:id' element={<Coin />} />
--- src/Routes/Coin.js
+++ src/Routes/Coin.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+const users = ['john', 'alice', 'alex', 'tim', 'bob'];
 
 function Coin() {
-  return <div></div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchTerm = searchParams.get('name') || '';
+
+  const handleSearch = event => {
+    const name = event.target.value;
+
+    if (name) {
+      setSearchParams({ name });
+    } else {
+      setSearchParams({});
+    }
+  };
+
+  return (
+    <>
+      <h1>Users</h1>
+      <input type='text' value={searchTerm} onChange={handleSearch} />
+      <ul>
+        {users
+          .filter((user) =>
+            user.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((user, i) => (
+            <li key={i}>{user}</li>
+          ))}
+      </ul>
+    </>
+  );
 }
 
 export default Coin;
