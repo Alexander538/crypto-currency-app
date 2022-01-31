0ec57694ac1b1b76a4ba6a848fdfa6d7eeb62d53
Alex Ferro <alexanderferro19@gmail.com>
2022-01-31 14:49:16 -0500

    styling for coin route

--- src/Routes/Coin.js
+++ src/Routes/Coin.js
@@ -7,20 +7,30 @@ function Coin() {
   const [coin, setCoin] = useState(null);
 
   useEffect(() => {
-      console.log(id);
-      Axios.get(`https://api.coinstats.app/public/v1/coins/${id}`).then(
-          (response) => {
-              console.log(response.data);
-              setCoin(response.data.coin);
-              console.log(coin)
-          }
-      );
+    console.log(id);
+    Axios.get(`https://api.coinstats.app/public/v1/coins/${id}`).then(
+      (response) => {
+        console.log(response.data);
+        setCoin(response.data.coin);
+        console.log(coin);
+      }
+    );
   }, []);
 
   if (coin) {
-      return <div> yo {coin.name} </div>
+    return (
+      <div className='coinView'>
+        <div className='coinCard'>
+          {' '}
+          <h1> Name: {coin.name} </h1>
+          <img src={coin.icon} />
+          <h3> Price: {coin.price}</h3>
+          <h3> Symbol: {coin.symbol}</h3>
+        </div>
+      </div>
+    );
   } else {
-      return <div> loading... </div>
+    return <div> loading... </div>;
   }
 }
 
